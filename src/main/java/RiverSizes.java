ccbce4862aa1368294f56d83da14bb1238abcace
João Francisco <joao.francisco@tst.jus.br>
2022-09-09 12:53:28 -0300

    Complete river sizes assignment

--- src/main/java/RiverSizes.java
+++ src/main/java/RiverSizes.java
@@ -11,7 +11,7 @@ public class RiverSizes {
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
                 if (matrix[i][j] == 1 && visited[i][j] == 0) {
-                    int size = findRiverSize(matrix, visited, n, m, i, j, 1);
+                    int size = findRiverSize(matrix, visited, i, j, 1);
                     rivers.add(size);
                 }
                 visited[i][j] = 1;
@@ -20,32 +20,30 @@ public class RiverSizes {
         return rivers;
     }
 
-    private static int findRiverSize(int[][] matrix, int[][] visited, int n, int m, int x, int y, int size) {
+    private static int findRiverSize(int[][] matrix, int[][] visited, int x, int y, int size) {
         visited[x][y] = 1;
-        if (x + 1 < n && visited[x + 1][y] == 0) {
-            visited[x + 1][y] = 1;
-            if (matrix[x + 1][y] == 1) {
-                size = findRiverSize(matrix, visited, n, m, x + 1, y, ++size);
-            }
+        if (isAdjacentRiverTile(matrix, visited, x + 1, y)) {
+            size = findRiverSize(matrix, visited, x + 1, y, size + 1);
         }
-        if (x - 1 > 0 && visited[x - 1][y] == 0) {
-            visited[x - 1][y] = 1;
-            if (matrix[x - 1][y] == 1) {
-                size = findRiverSize(matrix, visited, n, m, x - 1, y, ++size);
-            }
+        if (isAdjacentRiverTile(matrix, visited, x - 1, y)) {
+            size = findRiverSize(matrix, visited, x - 1, y, size + 1);
         }
-        if (y + 1 < m && visited[x][y + 1] == 0) {
-            visited[x][y + 1] = 1;
-            if (matrix[x][y + 1] == 1) {
-                size = findRiverSize(matrix, visited, n, m, x, y + 1, ++size);
-            }
+        if (isAdjacentRiverTile(matrix, visited, x, y + 1)) {
+            size = findRiverSize(matrix, visited, x, y + 1, size + 1);
         }
-        if (y - 1 > 0 && visited[x][y - 1] == 0) {
-            visited[x][y - 1] = 1;
-            if (matrix[x][y - 1] == 1) {
-                size = findRiverSize(matrix, visited, n, m, x, y - 1, ++size);
-            }
+        if (isAdjacentRiverTile(matrix, visited, x, y - 1)) {
+            size = findRiverSize(matrix, visited, x, y - 1, size + 1);
         }
         return size;
+    }
+
+    private static boolean isAdjacentRiverTile(int[][] matrix, int[][] visited, int x, int y) {
+        int n = matrix.length;
+        int m = matrix[0].length;
+        if (x < n && y < m && x >= 0 && y >= 0 && visited[x][y] == 0) {
+            visited[x][y] = 1;
+            return matrix[x][y] == 1;
+        }
+        return false;
     }
 }
